a85c71a4517120a42bfce99e0b7f34bde160e87d
KeithLamb <keithianlamb@gmail.com>
2024-05-20 12:47:21 -0500

    Create Slices for state management of Form and Order Components and complete store.js

---
+++ frontend/state/slices/formSlice.js
@@ -0,0 +1,54 @@
+import { createSlice, createAsyncThink } from '@reduxjs/toolkit'
+import axios from 'axios'
+
+export const postOrder = createAsyncThink('form/postOrder', async(order, { rejectWithValue }) => {
+    try {
+        const response = await axios.post('http://localhost:9009/api/pizza/order', order)
+        return response.data
+    } catch (err) {
+        return rejectWithValue(err.response.data)
+    }
+})
+
+const formSlice = createSlice({
+    name: 'form',
+    initialState: {
+        fullName: '',
+        size:'',
+        toppings: [],
+        status: 'idle',
+        error: null,
+    },
+    reducers: {
+        setFormValue: (state, action) => {
+            state[action.payload.name] = action.payload.value
+        },
+        toggleTopping: (state, action) => {
+            const topping = action.payload
+            if ( state.toppings.includes(topping)) {
+                state.toppings = state.toppings.filter((top) => top !== topping)
+            } else {
+                state.toppings.push(topping)
+            }
+        },
+    },
+    extraReducers: (builder) => {
+      builder
+        .addCase(postOrder.pending, (state) => {
+            state.status = 'loading'
+        })
+        .addCase(postOrder.pending, (state) => {
+            state.status = 'succeeded'
+            state.fullName = ''
+            state.size = ''
+            state.toppings = []
+        })
+        .addCase(postOrder.rejected, (state, action) => {
+            state.status = 'failed'
+            state.error = action.payload.message
+        })
+    }
+})
+
+export const { setFormValue, toggleTopping } = formSlice.actions
+export default formSlice.reducer
--- frontend/state/store.js
+++ frontend/state/store.js
@@ -1,18 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit'
-
-const exampleReducer = (state = { count: 0 }) => {
-  return state
-}
+import ordersReducer from './ordersSlice'
+import formReducer from './formSlice'
 
 export const resetStore = () => configureStore({
   reducer: {
-    example: exampleReducer,
+    orders: ordersReducer,
+    form: formReducer,
     // add your reducer(s) here
-  },
-  middleware: getDefault => getDefault().concat(
-    // if using RTK Query for your networking: add your middleware here
-    // if using Redux Thunk for your networking: you can ignore this
-  ),
+  }  // using Redux Thunk for your networking: you can ignore this
 })
 
 export const store = resetStore()
